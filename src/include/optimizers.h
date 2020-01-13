f32a4b1c385a1fbbd8a76062455d89f5db884ece
David <ddleon@outlook.co.il>
2020-01-13 13:03:27 -0800

    Momentum optimizer was added

--- src/include/optimizers.h
+++ src/include/optimizers.h
@@ -67,6 +67,56 @@ public:
 	}
 };
 
+/**
+ * Momentum Optimizer
+ * Accelerates SGD with momentum factor based on past step.
+ * v_ is the current velocity v(t)
+ * v_p_ is past velocity v(t-1)
+ * v(t) = gamma_*v(t-1)+lr*W_grad
+ * W = W-v(t)
+ */
+class Momentum : public Optimizer
+{
+private:
+	MatrixXd v_;
+	MatrixXd v_p_;
+
+	double gamma_;
+	double v_bias_;
+	double v_p_bias_;
+	bool init;
+
+public:
+
+	Momentum() : Optimizer() , gamma_(0.9),v_bias_(0),v_p_bias_(0),init(true)
+	{
+
+	}
+
+	void optimize(MatrixXd &Weights,const MatrixXd &W_grad, double &bias, const double bias_diff, double lr) override
+	{
+		if (init)
+		{
+			v_p_ = Eigen::MatrixXd::Zero(W_grad.rows(),W_grad.cols());
+			v_bias_ = bias_diff;
+			v_p_bias_ = 0;
+			init = false;
+		}
+
+		//calculating current v(t) and v_bias(t)
+		v_ = lr*W_grad+gamma_*v_p_;
+		v_bias_ = lr*bias_diff+gamma_*v_p_bias_;
+
+		//update weights and bias
+		Weights -= v_;//W-V(t)
+		bias -= v_bias_;//b-v(t)
+
+		//update past temrs for next calculation
+		v_p_ = v_;
+		v_p_bias_ = v_bias_;
+	}
+
+};
 
 
 
