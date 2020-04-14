31cea97b24cd35e977fd9785eea9d6b9f58f2d5f
David <ddleon@outlook.co.il>
2020-04-14 06:54:35 -0700

    test hook

--- src/include/ModelLoader.h
+++ src/include/ModelLoader.h
@@ -13,9 +13,10 @@
 #include "Model.h"
 #include <string>
 #include <unordered_map>
+#include <fstream>
+
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
-#include <fstream>
 
 using namespace boost::property_tree;
 using namespace SANN;
